1e957614feeceb9bfc7b001cf9fdab7e18c75cfa
nikkipruitt <nikkipruitt182@gmail.com>
2015-10-12 12:11:04 -0500

    Message saves to a file

--- NetworkingProject1/src/networkingproject1/TextInterface.java
+++ NetworkingProject1/src/networkingproject1/TextInterface.java
@@ -6,12 +6,20 @@
 
 package networkingproject1;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Nikki
  */
 public class TextInterface extends javax.swing.JFrame {
 
+    UDPSender sender;
+    UDPReceiver receiver;
+    
     /**
      * Creates new form TextInterface
      */
@@ -29,22 +37,37 @@ public class TextInterface extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        messageTextBox = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        IPAddressBox = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jTextField3 = new javax.swing.JTextField();
+        jTextField4 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setText("Message:");
 
-        jTextField1.setText("Enter your message here");
+        messageTextBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                messageTextBoxActionPerformed(evt);
+            }
+        });
 
         jLabel2.setText("IP address: ");
 
-        jTextField2.setText("Enter the IP adress");
-
         jButton1.setText("Send");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("Message:");
+
+        jLabel4.setText("Sent from:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -54,34 +77,67 @@ public class TextInterface extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel2)
-                    .addComponent(jLabel1))
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4))
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jButton1)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 182, Short.MAX_VALUE)
-                        .addComponent(jTextField2)))
+                    .addComponent(messageTextBox, javax.swing.GroupLayout.DEFAULT_SIZE, 182, Short.MAX_VALUE)
+                    .addComponent(IPAddressBox)
+                    .addComponent(jTextField3)
+                    .addComponent(jTextField4))
                 .addContainerGap(122, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(12, 12, 12)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(12, 12, 12)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(messageTextBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(IPAddressBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 59, Short.MAX_VALUE)
+                        .addComponent(jLabel3))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(jButton1)
-                .addContainerGap(173, Short.MAX_VALUE))
+                    .addComponent(jLabel4)
+                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(58, 58, 58))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void messageTextBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_messageTextBoxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_messageTextBoxActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        try {
+            // TODO add your handling code here:
+            writeToFile();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(TextInterface.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void writeToFile() throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter("message.txt");
+        writer.println(messageTextBox.getText());
+        writer.close();
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -115,13 +171,18 @@ public class TextInterface extends javax.swing.JFrame {
                 new TextInterface().setVisible(true);
             }
         });
+        
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField IPAddressBox;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField messageTextBox;
     // End of variables declaration//GEN-END:variables
 }
--- NetworkingProject1/src/networkingproject1/UDPReceiver.java
+++ NetworkingProject1/src/networkingproject1/UDPReceiver.java
@@ -7,44 +7,42 @@ import java.io.*;
 import java.util.*;
 
 public class UDPReceiver  implements Runnable{
-        @Override
-	public void run () {
-	  try{
-		// Create a datagram socket, bound to the specific port 2000
-		DatagramSocket socket = new DatagramSocket(2000);
+    @Override
+    public void run () {
+        try{
+            // Create a datagram socket, bound to the specific port 2000
+            DatagramSocket socket = new DatagramSocket(2000);
 
-		System.out.println ("Bound to local port " + socket.getLocalPort());
+            System.out.println ("Bound to local port " + socket.getLocalPort());
 
-		// Create a datagram packet, containing a maximum buffer of 256 bytes
-		DatagramPacket packet = new DatagramPacket( new byte[256], 256 );
+            // Create a datagram packet, containing a maximum buffer of 256 bytes
+            DatagramPacket packet = new DatagramPacket( new byte[256], 256 );
 
-		// Receive a packet - remember by default this is a blocking operation
-		socket.receive(packet);
+            // Receive a packet - remember by default this is a blocking operation
+            socket.receive(packet);
 
-		System.out.println ("Packet received at " + new Date( ));
-		// Display packet information
-		InetAddress remote_addr = packet.getAddress();
-		System.out.println ("Sender: " + remote_addr.getHostAddress( ) );
-		System.out.println ("from Port: " + packet.getPort());
+            System.out.println ("Packet received at " + new Date( ));
+            // Display packet information
+            InetAddress remote_addr = packet.getAddress();
+            System.out.println ("Sender: " + remote_addr.getHostAddress( ) );
+            System.out.println ("from Port: " + packet.getPort());
 
-		// Display packet contents, by reading from byte array
-		ByteArrayInputStream bin = new ByteArrayInputStream 
-(packet.getData());
+            // Display packet contents, by reading from byte array
+            ByteArrayInputStream bin = new ByteArrayInputStream (packet.getData());
 
-		// Display only up to the length of the original UDP packet
-		for (int i=0; i < packet.getLength(); i++)  {
-			int data = bin.read();
-			if (data == -1)
-				break;
-			else
-				System.out.print ( (char) data) ;
-		}
+            // Display only up to the length of the original UDP packet
+            for (int i=0; i < packet.getLength(); i++)  {
+                int data = bin.read();
+		if (data == -1)
+                    break;
+		else
+                    System.out.print ( (char) data) ;
+            }
 			
-		socket.close( );
+            socket.close( );
 	}
 	catch (IOException e) 	{
-		System.out.println ("Error - " + e);
+            System.out.println ("Error - " + e);
 	}
-
-   } //end of main
+   }
 } //end of class definition
